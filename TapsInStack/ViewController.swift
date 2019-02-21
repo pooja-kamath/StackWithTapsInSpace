8cb8508e91741410d1b79f9ec6b8a31a6c4b6b11
pooja-kamath <poojakamath91@gmail.com>
2019-02-21 21:12:47 +0530

    Add files via upload

---
+++ TapsInStack/ViewController.swift
@@ -0,0 +1,28 @@
+//
+//  ViewController.swift
+//  TapsInStack
+//
+//  Created by Audiology 5 on 21/02/19.
+//  Copyright © 2019 Edureka. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+    @IBAction func TappedSpace(_ sender: Any) {
+        
+    }
+    
+}
+
